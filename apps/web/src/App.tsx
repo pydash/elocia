a1c4897a0d266d9043e5783cad318bf7d5a62a38
Daniel Mark Sarabusing <142899403+pydash@users.noreply.github.com>
2026-08-19 22:12:51 +0800

    feat(web/App.tsx): add support for multiple user types (teacher, parent, admin) and their respective routes and layouts

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,27 +1,44 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Login from './pages/Login'
+import Landing from "./pages/Landing.tsx";
+import TeacherLogin from "./pages/teacher/Login.tsx";
+import ParentLogin from "./pages/parent/Login.tsx";
 import TeacherLayout from "./layouts/TeacherLayout.tsx";
-import TeacherDashboard from './pages/teacher/Dashboard'
-import ParentDashboard from './pages/parent/Dashboard'
-import AdminDashboard from './pages/admin/Dashboard'
+import TeacherStudents from "./pages/teacher/Students.tsx";
+import ParentLayout from "./layouts/ParentLayout.tsx";
+import ParentDashboard from "./pages/parent/Dashboard";
+import AdminDashboard from "./pages/admin/Dashboard";
+
+import TeacherLessons from "./pages/teacher/Lessons.tsx";
+import TeacherTasks from "./pages/teacher/Tasks.tsx";
+import TeacherSettings from "./pages/teacher/Settings.tsx";
+import TeacherHelp from "./pages/teacher/Help.tsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Landing />} />
+
+        <Route path="/teacher/login" element={<TeacherLogin />} />
+        <Route path="/parent/login" element={<ParentLogin />} />
 
         <Route path="/teacher" element={<TeacherLayout />}>
-           <Route index element={<TeacherDashboard />} />
+          <Route path="students" index element={<TeacherStudents />} />
+          <Route path="lessons" element={<TeacherLessons />} />
+          <Route path="tasks" element={<TeacherTasks />} />
+          <Route path="settings" element={<TeacherSettings />} />
+          <Route path="help" element={<TeacherHelp />} />
         </Route>
 
-        <Route path="/parent" element={<ParentDashboard />} />
+        <Route path="/parent" element={<ParentLayout />}>
+          <Route index element={<ParentDashboard />} />
+        </Route>
 
         <Route path="/admin" element={<AdminDashboard />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
