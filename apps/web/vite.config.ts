dafc0b6b3b6f70d477306b1f7552bb969231be86
Daniel Mark Sarabusing <142899403+pydash@users.noreply.github.com>
2026-09-01 10:43:06 +0800

    feat(config): add path alias for src directory and configure vite to use it fix(config): update vite config to use process.env.PORT for server port

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    port: 5174
-  }
-})
+    port: 5174,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
