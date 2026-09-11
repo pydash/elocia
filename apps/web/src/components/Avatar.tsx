70636fcd43dac6965f31c4ba855091401a6d8e0e
Daniel Mark Sarabusing <142899403+pydash@users.noreply.github.com>
2026-09-11 12:04:40 +0800

    style(web): increase font size for better visibility

--- apps/web/src/components/Avatar.tsx
+++ apps/web/src/components/Avatar.tsx
@@ -9,7 +9,7 @@ export default function Avatar({ emoji, color }: AvatarProps) {
       className={`flex h-24 w-24 items-center justify-center rounded-full text-(--primary) border-2`}
       style={{ backgroundColor: color }}
     >
-      <span className="text-2xl font-bold">{emoji}</span>
+      <span className="text-4xl font-bold">{emoji}</span>
     </div>
   );
 }
